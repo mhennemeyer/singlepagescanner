acc3aebd3f2e590e17b74af639f85713c7bf6623
Matthias Hennemeyer <mhennemeyer@gmail.com>
2020-11-05 23:50:46 +0100

    rectDetectRequest

--- Sources/SinglePageScanner/Common/VisionRectangleDetector.swift
+++ Sources/SinglePageScanner/Common/VisionRectangleDetector.swift
@@ -45,8 +45,8 @@ enum VisionRectangleDetector {
             rectDetectRequest.maximumObservations = 15
             rectDetectRequest.minimumConfidence = 0.1
             // FIXME: Inject values from Outside
-            rectDetectRequest.minimumAspectRatio = 0.66
-            rectDetectRequest.maximumAspectRatio = 0.73
+            rectDetectRequest.minimumAspectRatio = 0.64
+            rectDetectRequest.maximumAspectRatio = 0.75
             rectDetectRequest.quadratureTolerance = 20
             rectDetectRequest.minimumSize = 0.6
 
